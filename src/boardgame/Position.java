db16a20b5c347787d2edc677539e194b4c811ae4
Paulo <paulo_vinicius_21@hotmail.com>
2022-11-15 15:22:10 -0300

    Little improvement in board printing

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -3,38 +3,23 @@ package boardgame;
 public class Position {
 	private int row;
 	private int column;
-	
-	
-	
-	
+		
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
 
-
-
-
 	public int getRow() {
 		return row;
 	}
-
-
-
 
 	public void setRow(int row) {
 		this.row = row;
 	}
 
-
-
-
 	public int getColumn() {
 		return column;
 	}
-
-
-
 
 	public void setColumn(int column) {
 		this.column = column;
@@ -45,5 +30,4 @@ public class Position {
 		return row + ", " + column;
 	}
 	
-
 }
